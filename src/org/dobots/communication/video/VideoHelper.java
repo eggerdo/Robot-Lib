ae4533668ce0cad22d43724fdf1a67edc059a2f3
Dominik Egger <dominik@almende.org>
2013-08-30 15:11:20 +0200

    VideoHelper added. VideoSurfaceView revised.

---
+++ src/org/dobots/communication/video/VideoHelper.java
@@ -0,0 +1,254 @@
+/**
+* 456789------------------------------------------------------------------------------------------------------------120
+*
+* @brief: Helper class to display a video on the screen.
+* @file: VideoHelper.java
+*
+* @desc:  includes loading progress bar and a FPS view. a watchdog checks if frames are 
+* 		  received and in case of a timeout will reset connection status and display the 
+* 		  loading screen. same applies when connecting and no frame is received
+*
+*
+* Copyright (c) 2013 Dominik Egger <dominik@dobots.nl>
+*
+* @author:		Dominik Egger
+* @date:		30.08.2013
+* @project:		RobotLib
+* @company:		Distributed Organisms B.V.
+*/
+package org.dobots.communication.video;
+
+import org.dobots.R;
+import org.dobots.communication.video.VideoSurfaceView.DisplayMode;
+import org.dobots.utilities.Utils;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class VideoHelper implements IVideoListener, IFpsListener, IRawVideoListener {
+	
+	private static final int CONNECTION_TIMEOUT = 15000; // 15 seconds
+	private static final int WATCHDOG_TIMEOUT = 5000; // 5 seconds
+	private static final int WATCHDOG_INTERVAL = 1000; // 1 second
+	
+	private Handler mHandler = new Handler();
+
+	private boolean m_bVideoConnected;
+
+	private boolean m_bVideoStopped;
+
+	private VideoSurfaceView m_ivVideo;
+	private ProgressBar m_pbLoading;
+	private TextView m_lblFps;
+	private FrameLayout m_layCamera;
+
+	private Activity m_oActivity;
+	
+	private long lastFrameRecv = 0;
+
+	/**
+	 * takes an activity and a view container as input, then places a camera view, a progress bar
+	 * and a FPS view inside the container
+	 * @param activity the activity in which the video should be shown
+	 * @param container the container where the video should be shown
+	 */
+	public VideoHelper(Activity activity, ViewGroup container) {
+		m_oActivity = activity;
+		
+    	LayoutInflater.from(activity).inflate(R.layout.videoview, container);
+    	m_layCamera = (FrameLayout) container.findViewById(R.id.layCamera);
+    	m_ivVideo = (VideoSurfaceView) container.findViewById(R.id.ivCamera);
+    	m_pbLoading = (ProgressBar) container.findViewById(R.id.pbLoading);
+    	m_lblFps = (TextView) container.findViewById(R.id.lblFPS);
+    	
+    	m_ivVideo.setDisplayMode(DisplayMode.SCALED);
+    	
+    	initialize();
+    	
+    	mHandler.postDelayed(m_oWatchdog, WATCHDOG_INTERVAL);
+	}
+	
+	/**
+	 * call when activity is destroyed
+	 */
+	public void onDestroy() {
+		m_ivVideo.onDestroy();
+	}
+	
+	// the watchdog timer checks if new frames are received. if no new frame
+	// is received within a given period, the VideoConnected flag is cleared
+	// and the loading screen is shown until a frame is received again
+	protected Runnable m_oWatchdog = new Runnable() {
+		
+		public void run() {
+			if (m_bVideoConnected && !m_bVideoStopped) {
+				if (System.currentTimeMillis() - lastFrameRecv > WATCHDOG_TIMEOUT) {
+					m_bVideoConnected = false;
+					showVideoLoading(true);
+				}
+			}
+			mHandler.postDelayed(this, WATCHDOG_INTERVAL);
+		}
+	};
+
+	// the timeout runnable is used if connection is established. if no frame
+	// is received when the timeout triggers we assume that video connection failed.
+	protected Runnable m_oTimeoutRunnable = new Runnable() {
+		@Override
+		public void run() {
+			if (!m_bVideoConnected) {
+				m_bVideoStopped = true;
+				showVideoLoading(false);
+				showVideoMsg("Video Connection Failed");
+			}
+		}
+	};
+
+	// TODO: neccessary?
+	public void resetLayout() {
+		initialize();
+		
+		Utils.showView(m_ivVideo, false);
+	}
+	
+	private void initialize() {
+		m_bVideoConnected = false;
+		m_bVideoStopped = false;
+	}
+
+
+	/**
+	 * call once video was started, check for incoming frames with timeout
+	 * set to true 
+	 */
+	public void onStartVideo() {
+		onStartVideo(true);
+	}
+	
+	
+	/**
+	 * call once video was started, waits for incoming frames
+	 * @param i_bTimeout if true and no frame is coming in within a timeout
+	 * 					 then connection is assumed to have failed and the
+	 * 					 flags are reset
+	 */
+	public void onStartVideo(boolean i_bTimeout) {
+		m_bVideoConnected = false;
+		m_bVideoStopped = false;
+		showVideoLoading(true);
+		if (i_bTimeout) {
+			mHandler.postDelayed(m_oTimeoutRunnable, CONNECTION_TIMEOUT);
+		}
+	}
+
+	/**
+	 * call once video was stopped
+	 */
+	public void onStopVideo() {
+		m_bVideoStopped = true;
+		showVideoMsg("Video OFF");
+	}
+	
+	/**
+	 * shows/hides the video and video loading progress
+	 * @param i_bShow if false, the video is shown and progress is hidden
+	 * 					 true, the video is hidden and progress is shown
+	 */
+	protected void showVideoLoading(final boolean i_bShow) {
+		mHandler.post(new Runnable() {
+			@Override
+			public void run() {
+				Utils.showView(m_ivVideo, !i_bShow);
+				Utils.showView(m_pbLoading, i_bShow);
+			}
+		});
+	}
+
+	/**
+	 * write the given message on the video screen
+	 * @param i_strMsg message to be displayed
+	 */
+	protected void showVideoMsg(String i_strMsg) {
+		int width = m_layCamera.getWidth();
+		int height = m_layCamera.getHeight();
+		
+		if (width == 0 || height == 0) {
+			width = 380;
+			height = 240;
+		}
+		
+		Utils.writeToSurfaceView(m_oActivity, m_ivVideo, i_strMsg, true);
+	}
+
+	/**
+	 * callback to update frames per second on display
+	 */
+	@Override
+	public void onFPS(final int i_nFPS) {
+		Utils.runAsyncUiTask(new Runnable() {
+
+			@Override
+			public void run() {
+				if (!m_bVideoStopped) {
+					m_lblFps.setText("FPS: " + String.valueOf(i_nFPS));
+				}
+			}
+		});
+		
+	}
+
+	/**
+	 * callback when already decoded bitmap should be shown
+	 */
+	@Override
+	public void onFrame(Bitmap bmp, int rotation) {
+		if (!m_bVideoStopped) {
+			if (!m_bVideoConnected) {
+				// if this is the first frame received, cancel the timeout,
+				// set flag to connected and show the video
+				mHandler.removeCallbacks(m_oTimeoutRunnable);
+				m_bVideoConnected = true;
+				showVideoLoading(false);
+			}
+			
+			// store current time for watchdog
+			lastFrameRecv = System.currentTimeMillis();
+			m_ivVideo.onFrame(bmp, rotation);
+		}
+	}
+
+	/**
+	 * callback when a frame is received as a byte array. will be decoded
+	 * by the VideoSurfaceView
+	 */
+	@Override
+	public void onFrame(byte[] rgb, int rotation) {
+		if (!m_bVideoStopped) {
+			Utils.runAsyncUiTask(new Runnable() {
+				
+				@Override
+				public void run() {
+					if (!m_bVideoConnected) {
+						// if this is the first frame received, cancel the timeout,
+						// set flag to connected and show the video
+						mHandler.removeCallbacks(m_oTimeoutRunnable);
+						m_bVideoConnected = true;
+						showVideoLoading(false);
+					}
+				}
+			});
+
+			// store current time for watchdog
+			lastFrameRecv = System.currentTimeMillis();
+			m_ivVideo.onFrame(rgb, rotation);
+
+		}
+	}
+    
+}
---
+++ src/org/dobots/communication/video/VideoSurfaceView.java
@@ -0,0 +1,268 @@
+/**
+* 456789------------------------------------------------------------------------------------------------------------120
+*
+* @brief: VideoSurfaceView to display a Video.
+* @file: VideoSurfaceView.java
+*
+* @desc:  receives video frames and draws them on the surface. depending on the current display mode
+* 		  the frame will be scaled appropriately on the screen. The frame is also rotated if necessary
+* 		  so that orientation is correct.
+*
+*
+* Copyright (c) 2013 Dominik Egger <dominik@dobots.nl>
+*
+* @author:		Dominik Egger
+* @date:		30.08.2013
+* @project:		RobotLib
+* @company:		Distributed Organisms B.V.
+*/
+package org.dobots.communication.video;
+
+import java.io.ByteArrayInputStream;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceView;
+
+public class VideoSurfaceView extends SurfaceView implements IVideoListener, IRawVideoListener {
+
+	public static final String TAG = "VideoSurfaceView";
+
+	/**
+	 * Mode in which the frame should be displayed on the screen:
+	 *   NORMAL: the frame will be displayed with the dimensions as received
+	 *	 SCALED: the frame will be scaled so that it fits inside the given space. proportions will be kept
+	 *   FILLED: the frame will be scaled so that it fills the given space. proportions will be discarded.
+	 */
+	public enum DisplayMode {
+		NORMAL, SCALED, FILLED
+	}
+	
+	private DisplayMode mMode;
+	
+	// limit the width and height if it is set to scale
+	private int m_nMaxWidth = 0;
+	private int m_nMaxHeight = 0;
+	
+	// dispHeight and dispWidth are the dimensions in which the video should be displayed on screen
+	int mDispHeight = -1, mDispWidth = -1;
+	// bmpWidth and bmpHeight are the actual dimensions of the received frame
+	int mBmpWidth = -1, mBmpHeight = -1;
+	
+	private Paint mPainter;
+	
+	private boolean mSizeInitialized = false;
+
+	private Bitmap mCurrentFrame = null;
+	private byte[] mCurrentRgb = null;
+	private int mCurrentRotation = 0;
+	private Object mLock = new Object();
+	
+	public VideoSurfaceView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mPainter = new Paint();
+		mPainter.setAntiAlias(true);
+		mPainter.setFilterBitmap(true);
+		//		paint.setDither(true);
+		mPainter.setColor(Color.BLACK);
+		
+		mFrameDecoder.start();
+	}
+	
+	public void onDestroy() {
+		mFrameDecoder.interrupt();
+	}
+	
+	public void setDisplayMode(DisplayMode eMode) {
+		mMode = eMode;
+		mSizeInitialized = false;
+//		requestLayout();
+	}
+
+	public void setMaxWidth(int i_nMaxWidth) {
+		m_nMaxWidth = i_nMaxWidth;
+	}
+
+	public void setMaxHeight(int i_nMaxHeight) {
+		m_nMaxHeight = i_nMaxHeight;
+	}
+
+	// with slower phones it is most likely that decoding, rotating and scaling
+	// a frame takes longer than receiving frames. to avoid delay building up
+	// the receiving of frames is decoupled from the decoding. the received frame
+	// is stored in a variable. the decoder then always takes the currently stored
+	// frame. if frames are received faster than they are decoded, the old ones will
+	// be dropped.
+	public void onFrame(byte[] rgb, int rotation) {
+		
+		synchronized (mLock) {
+			mCurrentRgb = rgb;
+			mCurrentRotation = rotation;
+		}
+	}
+
+	// Thread responsible for creating a Bitmap out of the received Frame. 
+	Thread mFrameDecoder = new Thread("FrameDecoder") {
+		
+		public void run() {
+			
+			byte[] rgb = null;
+			int rotation = 0;
+
+			while (!isInterrupted()) {
+				synchronized (mLock) {
+					// if no frame was received, skip
+					// otherwise store it in a local variable so that
+					// we don't have to worry about it being overwritten
+					if (mCurrentRgb == null) {
+						continue;
+					} else {
+						rgb = mCurrentRgb;
+						rotation = mCurrentRotation;
+						mCurrentRgb = null;
+					}
+				}
+				
+				try {
+					ByteArrayInputStream stream = new ByteArrayInputStream(rgb);
+					Bitmap frame = BitmapFactory.decodeStream(stream);
+					
+					// if not already, initialize the size in which the frame should
+					// be displayed
+					if (!mSizeInitialized) {
+						setSize(frame, rotation);
+//						invalidate();
+//						requestLayout();
+					} 
+					
+					// recycle the old frame before assigning the new
+					if (mCurrentFrame != null) {
+						mCurrentFrame.recycle();
+					}
+					mCurrentFrame = frame;
+					
+					// draw the frame on the screen
+					drawVideoFrame(mCurrentFrame, rotation);
+			
+					counter.tick();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+		};
+	};
+	
+	FpsCounter counter = new FpsCounter(new IFpsListener() {
+		
+		@Override
+		public void onFPS(int i_nFPS) {
+			Log.d("ZmqVideo", String.format("fps dec: %d", i_nFPS));
+		}
+	});	
+
+	private void setSize(Bitmap bmp, int rotation) {
+		// if frame is rotated by 90 or 270 we need to swap
+		// height and width
+		if (rotation % 180 == 0) {
+			mBmpWidth = bmp.getWidth();
+			mBmpHeight = bmp.getHeight();
+		} else {
+			mBmpHeight = bmp.getWidth();
+			mBmpWidth = bmp.getHeight();
+		}
+		
+		mDispWidth = getWidth();
+		mDispHeight = getHeight();
+
+		// if height or width is 0 then the surface was not created yet (or the
+		// view has wrong layout parameters). in any case we keep trying to
+		// initialize the sizes.
+		if ((mDispHeight & mDispWidth) == 0) {
+			mSizeInitialized = false;
+			return;
+		}
+		
+		switch(mMode) {
+		case SCALED:
+			// check if max width / height is set
+			if (m_nMaxWidth != 0) {
+				mDispWidth = Math.min(mDispWidth, m_nMaxWidth);
+			}
+			if (m_nMaxHeight != 0) {
+				mDispHeight = Math.min(mDispHeight, m_nMaxHeight);
+			}
+			
+			// scale the height based on the width
+			mDispHeight = (int)(mBmpHeight * (double)mDispWidth / mBmpWidth);
+			
+			// if the height would be greater than the available height
+			// we rescale the width based on the height
+			if (mDispHeight > getHeight()) {
+				mDispHeight = getHeight();
+				mDispWidth = (int)(mBmpWidth * (double)mDispHeight / mBmpHeight);
+			}
+			break;
+		case FILLED:
+			// nothing to do, we just keep the width and height values already assigned above
+			break;
+		default:
+			// we display the frame as it is
+			mDispHeight = mBmpHeight;
+			mDispWidth = mBmpWidth;
+			
+		}
+		
+		mSizeInitialized = true;
+	}
+	
+	// draw the frame on the screen
+	private void drawVideoFrame(Bitmap bmp, int rotation) {
+		Canvas canvas = getHolder().lockCanvas();
+		if (canvas != null) {
+			try {
+				// get the center of the available space
+				float centerX, centerY;
+				centerX = (float) getWidth() / 2;
+				centerY = (float) getHeight() / 2;
+				
+				// calculate the top left corner where the (unrotated / unscaled) frame should
+				// be drawn so that it is centered in the available space
+				float left, top;
+				left = centerX - (bmp.getWidth() / 2);
+				top = centerY - (bmp.getHeight() / 2);
+
+				// execution of the transformation is inverted. so first after drawing the image it is
+				// rotated around the center of the image, then it is scaled (if necessary)
+				canvas.scale((float) mDispWidth / mBmpWidth, (float) mDispHeight / mBmpHeight, centerX, centerY);
+				canvas.rotate(rotation, centerX, centerY);
+				canvas.drawBitmap(bmp, left, top, mPainter);
+			} finally {
+				getHolder().unlockCanvasAndPost(canvas);
+			}
+		} else {
+			Log.e(TAG, "failed to get canvas!");
+//			invalidate();
+//			requestLayout();
+		}
+	}
+
+	// if the frame is received as bmp, calculate the size, then
+	// draw it on the screen.
+	@Override
+	public void onFrame(Bitmap bmp, int rotation) {
+		mCurrentFrame = bmp;
+		if (!mSizeInitialized) {
+			setSize(bmp, rotation);
+		}
+		drawVideoFrame(bmp, rotation);
+	}
+
+}
